c58e77f0167fb83c3e182b1c49745513a7643f75
HerbertSousa <herberti1405@outlook.com>
2021-06-24 11:58:53 -0300

    Component TopPanel

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <template>
 	<div id="pokedex">
-		<h1>Hello</h1>
+		<Pokedex />
 	</div>
 </template>
 
 <script>
+	import Pokedex from './components/Pokedex.vue';
 
 	export default {
 		name: 'App',
+		components: {
+			Pokedex,
+		},
 	};
 </script>
 
 <style lang="scss" scoped>
-#pokedex {
-	display: flex;
-	flex-direction: column;
-	min-height: 100vh;
-	max-width: 0 auto;
-	padding: 24px;
-}
+	#pokedex {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: calc(100vh - 20px);
+		padding: 20px;
+	}
 </style>
